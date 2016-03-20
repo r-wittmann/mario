c074c6619b809b0328fe2a2a8046a612f0f01f95
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-03-20 16:45:29 +0100

    restructure project and add center button

---
+++ app.js
@@ -0,0 +1,25 @@
+/* global angular */
+var app = angular.module('app', ['leaflet-directive'])
+
+app.controller('Ctrl', [ '$scope', function ($scope) {
+  angular.extend($scope, {
+    center: {
+      lat: 48.137,
+      lng: 11.575,
+      zoom: 12
+    },
+    defaults: {
+      maxZoom: 20,
+      minZoom: 6
+    }
+  })
+  $scope.btnClick = function () {
+    angular.extend($scope, {
+      center: {
+        lat: 48.137,
+        lng: 11.575,
+        zoom: 12
+      }
+    })
+  }
+}])
