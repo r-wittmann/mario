f54600762ee87f416c57486b1eebef3e7054b702
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-03-28 16:38:08 +0200

    add geoJson Layer with possibility of adding, centering and removing Layer

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-/* global angular */
-var app = angular.module('app', ['leaflet-directive'])
+/* global angular L */
+let app = angular.module('app', ['leaflet-directive'])
 
-app.controller('Ctrl', [ '$scope', function ($scope) {
+app.controller('Ctrl', [ '$scope', '$http', 'leafletData', function ($scope, $http, leafletData) {
   angular.extend($scope, {
     center: {
       lat: 48.137,
@@ -14,13 +14,38 @@ app.controller('Ctrl', [ '$scope', function ($scope) {
     }
   })
 
-  $scope.btnClick = function () {
-    angular.extend($scope, {
-      center: {
-        lat: 48.137,
-        lng: 11.575,
-        zoom: 12
+  $scope.getTestData = function () {
+    getTestData()
+  }
+
+  $scope.centerOnRoute = function () {
+    leafletData.getMap().then(function (map) {
+      if ($scope.geojson) {
+        let latlngs = []
+        for (let i in $scope.geojson.data.features) {
+          for (let j in $scope.geojson.data.features[i].geometry.coordinates) {
+            let coord = $scope.geojson.data.features[i].geometry.coordinates[j]
+            latlngs.push(L.GeoJSON.coordsToLatLng(coord))
+          }
+        }
+        map.fitBounds(latlngs)
       }
+    })
+  }
+
+  $scope.removeRoute = function () {
+    if ($scope.geojson) {
+      delete $scope.geojson
+    }
+  }
+
+  function getTestData () {
+    $http.get('/mocks/geoJsonMock.geo.json').then(function (response) {
+      angular.extend($scope, {
+        geojson: {
+          data: response.data
+        }
+      })
     })
   }
 }])
