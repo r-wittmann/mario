bc049f8beff37e81f60e9878553b210661ac7473
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-03-28 22:15:54 +0200

    add functionality for start and destination marker and calculate dummy route

--- js/app.js
+++ js/app.js
@@ -13,16 +13,46 @@ app.controller('Ctrl', [ '$scope', '$http', 'leafletData', 'leafletMapEvents', f
     },
     defaults: {
       maxZoom: 20,
-      minZoom: 6
+      minZoom: 0
     }
   })
 
-  $scope.$on('leafletDirectiveMap.click', function (event) {
-    console.log('click')
+  $scope.geojson = []
+  $scope.markers = []
+
+  $scope.$on('leafletDirectiveMap.click', function (event, args) {
+    if ($scope.markers.length < 2) {
+      $scope.markers.push({
+        lat: args.leafletEvent.latlng.lat,
+        lng: args.leafletEvent.latlng.lng,
+        draggable: true,
+        message: $scope.markers.length === 0 ? 'Start' : 'Ziel',
+        focus: true
+      })
+    }
   })
 
   $scope.getTestData = function () {
     getTestData()
+  }
+
+  $scope.calculateRoute = function () {
+    angular.extend($scope, {
+      geojson: {
+        data: {
+          'type': 'FeatureCollection',
+          'features': [{
+            'type': 'Feature',
+            'geometry': {
+              'type': 'LineString',
+              'coordinates': [
+                [$scope.markers[0].lng, $scope.markers[0].lat],
+                [$scope.markers[1].lng, $scope.markers[1].lat]]
+            }
+          }
+        ]}
+      }
+    })
   }
 
   $scope.centerOnRoute = function () {
@@ -44,6 +74,7 @@ app.controller('Ctrl', [ '$scope', '$http', 'leafletData', 'leafletMapEvents', f
     if ($scope.geojson) {
       delete $scope.geojson
     }
+    $scope.markers = []
   }
 
   function getTestData () {
