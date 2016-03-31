7735f1b60fe24583663025dd92e99e13884a3a92
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-04-01 00:17:47 +0200

    refactor app

--- js/app.js
+++ js/app.js
@@ -2,25 +2,42 @@
     angular
     L
 */
-let app = angular.module('app', ['leaflet-directive'])
+let mario = angular.module('app', ['leaflet-directive'])
 
-app.controller('Ctrl', [ '$scope', '$http', 'leafletData', 'leafletMapEvents', function ($scope, $http, leafletData, leafletMapEvents) {
-  angular.extend($scope, {
-    center: {
-      lat: 48.137,
-      lng: 11.575,
-      zoom: 12
-    },
-    defaults: {
-      maxZoom: 20,
-      minZoom: 0
+/**
+ * main controller of mario app
+ **/
+
+mario.controller('Ctrl',
+  ['$scope', 'leafletMapEvents', 'modifyMap', 'handleServerRequest', 'config',
+  function ($scope, leafletMapEvents, modifyMap, handleServerRequest, config) {
+    angular.extend($scope, config.map)
+
+    $scope.getTestData = function () {
+      handleServerRequest.getMock($scope)
     }
-  })
 
-  $scope.geojson = []
-  $scope.markers = []
+    $scope.centerOnRoute = function () {
+      modifyMap.centerOnRoute($scope)
+    }
 
-  $scope.$on('leafletDirectiveMap.click', function (event, args) {
+    $scope.removeElements = function () {
+      modifyMap.removeMarker($scope)
+      modifyMap.removeRoute($scope)
+    }
+
+    $scope.$on('leafletDirectiveMap.click', function (event, args) {
+      modifyMap.addMarker($scope, event, args)
+    })
+  }]
+)
+
+/**
+ * all map modification will be in this service
+ **/
+
+mario.service('modifyMap', ['leafletData', function (leafletData) {
+  this.addMarker = function ($scope, event, args) {
     if ($scope.markers.length < 2) {
       $scope.markers.push({
         lat: args.leafletEvent.latlng.lat,
@@ -30,60 +47,80 @@ app.controller('Ctrl', [ '$scope', '$http', 'leafletData', 'leafletMapEvents', f
         focus: true
       })
     }
-  })
-
-  $scope.getTestData = function () {
-    getTestData()
   }
 
-  $scope.calculateRoute = function () {
-    angular.extend($scope, {
-      geojson: {
-        data: {
-          'type': 'FeatureCollection',
-          'features': [{
-            'type': 'Feature',
-            'geometry': {
-              'type': 'LineString',
-              'coordinates': [
-                [$scope.markers[0].lng, $scope.markers[0].lat],
-                [$scope.markers[1].lng, $scope.markers[1].lat]]
-            }
-          }
-        ]}
-      }
-    })
-  }
-
-  $scope.centerOnRoute = function () {
-    leafletData.getMap().then(function (map) {
-      if ($scope.geojson) {
-        let latlngs = []
-        for (let i in $scope.geojson.data.features) {
-          for (let j in $scope.geojson.data.features[i].geometry.coordinates) {
-            let coord = $scope.geojson.data.features[i].geometry.coordinates[j]
-            latlngs.push(L.GeoJSON.coordsToLatLng(coord))
-          }
-        }
-        map.fitBounds(latlngs)
-      }
-    })
-  }
-
-  $scope.removeRoute = function () {
-    if ($scope.geojson) {
-      delete $scope.geojson
-    }
+  this.removeMarker = function ($scope) {
     $scope.markers = []
   }
 
-  function getTestData () {
-    $http.get('mocks/geoJsonMock.geo.json').then(function (response) {
-      angular.extend($scope, {
-        geojson: {
-          data: response.data
+  this.addRoute = function ($scope, data) {
+    angular.extend($scope, {
+      geojson: {
+        data: data
+      }
+    })
+  }
+
+  this.removeRoute = function ($scope) {
+    $scope.geojson = []
+  }
+
+  this.centerOnRoute = function ($scope) {
+    leafletData.getMap().then(function (map) {
+      let latlngs = []
+      for (let i in $scope.geojson.data.features) {
+        for (let j in $scope.geojson.data.features[i].geometry.coordinates) {
+          let coord = $scope.geojson.data.features[i].geometry.coordinates[j]
+          latlngs.push(L.GeoJSON.coordsToLatLng(coord))
         }
-      })
+      }
+      map.fitBounds(latlngs)
     })
   }
 }])
+
+/**
+ * all requests to the server will be handled here
+ **/
+
+mario.service('handleServerRequest', ['$http', 'handleServerResponse', function ($http, handleServerResponse) {
+  this.getMock = function ($scope) {
+    $http.get('mocks/geoJsonMock.geo.json').then(function (response) {
+      handleServerResponse.mockResponse($scope, response)
+    })
+  }
+}])
+
+/**
+ * all responses from the server will be handled here
+ **/
+
+mario.service('handleServerResponse', function () {
+  this.mockResponse = function ($scope, response) {
+    angular.extend($scope, {
+      geojson: {
+        data: response.data
+      }
+    })
+  }
+})
+
+/**
+ * all constants are declared in config-service
+ **/
+
+mario.service('config', function () {
+  this.map = {
+    center: {
+      lat: 48.137,
+      lng: 11.575,
+      zoom: 12
+    },
+    defaults: {
+      maxZoom: 15,
+      minZoom: 5
+    },
+    geojson: [],
+    markers: []
+  }
+})
