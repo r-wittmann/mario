b8e1d22a17c42b572e7f33b37158973c7752aaad
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-04-07 17:16:35 +0200

    tidy up code and structure

--- js/services/GeoCodeService.js
+++ js/services/GeoCodeService.js
@@ -1,12 +1,11 @@
-/* global mario*/
+/* global mario */
 
 mario.service('reverseGeocode', [ '$http', 'addToScope', function ($http, addToScope) {
   this.reverseMarker = function ($scope, index) {
     $http.get('http://nominatim.openstreetmap.org/reverse?format=json&lat=' +
-    $scope.markers[index].lat +
-    '&lon=' +
-    $scope.markers[index].lng +
-    '&zoom=18&addressdetails=1')
-    .then(function (response) { addToScope.addAddress($scope, index, response.data.address) })
+      $scope.markers[index].lat + '&lon=' + $scope.markers[index].lng + '&zoom=18&addressdetails=1')
+    .then(function (response) {
+      addToScope.addAddress($scope, index, response.data.address)
+    })
   }
 }])
