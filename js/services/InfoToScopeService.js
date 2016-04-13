b4f047567a248c2ef084a8c14bdcc095978fd953
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-04-13 10:52:14 +0200

    hack the osm data again

--- js/services/InfoToScopeService.js
+++ js/services/InfoToScopeService.js
@@ -3,10 +3,10 @@
 mario.service('addToScope', function () {
   this.addAddress = function ($scope, index, data) {
     $scope.route.startEndPoint[index] = {
-      street: data.road,
+      street: `${data.road ? data.road : data.cycleway}`,
       streetNumber: data.house_number,
       postalCode: data.postcode,
-      city: `${data.city ? data.city : data.town ? data.town : data.state}`
+      city: `${data.city ? data.city : data.town ? data.town : data.village ? data.village : data.state}`
     }
   }
 })
