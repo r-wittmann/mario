9efdb9340f74d0ddd09ec8032120b84a5576332c
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-04-14 20:16:36 +0200

    get calculated route from server

--- js/services/ServerConnectServices.js
+++ js/services/ServerConnectServices.js
@@ -5,17 +5,16 @@
  **/
 
 mario.service('handleServerRequest', ['$http', 'handleServerResponse', function ($http, handleServerResponse) {
+  let baseUrl = 'http://129.187.228.18:8080/restservices_path/webresources/'
+
   this.getInitialInformation = function ($scope) {
+    /* this file should be fetched from the server as well */
     $http.get('mocks/algorithms.json').then(function (response) {
       handleServerResponse.mockAlgorithms($scope, response)
     })
   }
 
   this.calculateRoute = function ($scope) {
-    console.log('called calculateRoute($scope), not implemented yet')
-    console.log('start:', $scope.markers[0], '\ntarget:', $scope.markers[1])
-    console.log('selected algorithm:', $scope.selected.algorithm, '\nselected cost:', $scope.selected.cost)
-    /*
     let startTarget = {
       'start': {
         'lat': $scope.markers[0].lat,
@@ -26,11 +25,17 @@ mario.service('handleServerRequest', ['$http', 'handleServerResponse', function 
         'lon': $scope.markers[1].lng
       },
       'algo': $scope.selected.algorithm
-      'cost': $scope.selected.cost
+      /* 'cost': $scope.selected.cost */
     }
     angular.toJson(startTarget)
-    $http.get('http://129.187.228.18:8080/restservices_path/webresources/easyev?', startTarget)
-    */
+    $http.post(baseUrl + 'easyev?', startTarget).then(function (response) {
+      handleServerResponse.directRouteResponse($scope, response)
+    })
+  }
+
+  this.calculateIntermodal = function ($scope) {
+    console.log('called calculateIntermodal($scope), not implemented yet')
+    console.log('start:', $scope.markers[0], '\ntarget:', $scope.markers[1])
   }
 }])
 
@@ -38,12 +43,16 @@ mario.service('handleServerRequest', ['$http', 'handleServerResponse', function 
  * all responses from the server will be handled here
  **/
 
-mario.service('handleServerResponse', [ function () {
+mario.service('handleServerResponse', [ 'modifyMap', function (modifyMap) {
   this.mockAlgorithms = function ($scope, response) {
     angular.extend($scope, response.data)
     $scope.selected = {
-      'algorithm': response.data.algorithms[0],
+      'algorithm': response.data.algorithms[1],
       'cost': response.data.algorithmCosts[0]
     }
+  }
+
+  this.directRouteResponse = function ($scope, response) {
+    modifyMap.addRoute($scope, response)
   }
 }])
