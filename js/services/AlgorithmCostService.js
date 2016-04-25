65dd2a01fc60ad98d3250b7a789bf0fee529115e
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-04-25 16:07:20 +0200

    create AlgorithmCostService

---
+++ js/services/AlgorithmCostService.js
@@ -0,0 +1,29 @@
+/* global angular mario */
+
+mario.service('algorithmCost', [ function () {
+  this.initialSave = function (model, response) {
+    angular.extend(model, response.data, {
+      selected: {
+        algorithm: response.data.algorithms[1],
+        costs: [response.data.algorithmCosts[0]]
+      }
+    })
+  }
+  this.selectAlgo = function (model, algorithm) {
+    model.selected.costs = [model.selected.costs[0]]
+  }
+  this.selectCost = function (model, cost) {
+    if (model.selected.algorithm.indexOf('skyline') >= 0) {
+      if (model.selected.costs.length <= 1 && model.selected.costs[0] === cost) {
+        return
+      } else if (model.selected.costs.indexOf(cost) === -1) {
+        model.selected.costs.push(cost)
+      } else {
+        model.selected.costs.splice(model.selected.costs.indexOf(cost), 1)
+      }
+    } else {
+      model.selected.costs.pop()
+      model.selected.costs.push(cost)
+    }
+  }
+}])
