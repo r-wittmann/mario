161e6da8d7ad6584cd0ab74c81bcdc8fd25a58fb
Rainer Wittmann <rainer.wittmann@comsysto.com>
2016-05-23 18:03:21 +0200

    improve address selection to use nearest address and best match

--- js/services/GeoCodeService.js
+++ js/services/GeoCodeService.js
@@ -17,10 +17,13 @@ mario.service('reverseGeocode', [ '$http', function ($http) {
     // })
     $http.get(hereUrl + 'prox=' +
       model.map.markers[index].lat + ',' +
-      model.map.markers[index].lng + ',500&app_id=' + App_Id + '&app_code=' + App_Code +
-      '&mode=retrieveAddresses&gen=9&language=en&maxresults=1')
+      model.map.markers[index].lng + ',1000&app_id=' + App_Id + '&app_code=' + App_Code +
+      '&mode=retrieveAddresses&gen=9&language=en&maxresults=5')
     .then(function (response) {
-      model.map.markers[index].formattedAddress = response.data.Response.View[0].Result[0].Location.Address.Label.split(', ')
+      let i = 0
+      if (response.data.Response.View[0].Result[0]['MatchLevel'] === 'houseNumber') i = 0
+      else if (response.data.Response.View[0].Result.length >= 2 && response.data.Response.View[0].Result[1].Location.Address.Street) i = 1
+      model.map.markers[index].formattedAddress = response.data.Response.View[0].Result[i].Location.Address.Label.split(', ')
       model.map.markers[index].formattedAddress.pop()
       model.infoDrop = true
     })
@@ -28,16 +31,18 @@ mario.service('reverseGeocode', [ '$http', function ($http) {
 
   this.reverseInstructions = function (model) {
     for (let feature of model.map.geojson.data.features) {
-      for (let i in feature.properties.instructions) {
-        if (feature.properties.instructions[i] instanceof Array) {
+      for (let index in feature.properties.instructions) {
+        if (feature.properties.instructions[index] instanceof Array) {
           $http.get(hereUrl + 'prox=' +
-            feature.properties.instructions[i]['0'] + ',' +
-            feature.properties.instructions[i]['1'] + ',500&app_id=' + App_Id + '&app_code=' + App_Code +
-            '&mode=retrieveAddresses&gen=9&language=en&maxresults=1')
+            feature.properties.instructions[index]['0'] + ',' +
+            feature.properties.instructions[index]['1'] + ',1000&app_id=' + App_Id + '&app_code=' + App_Code +
+            '&mode=retrieveAddresses&gen=9&language=en&maxresults=5')
           .then(function (response) {
-            feature.properties.instructions[i] = response.data.Response.View[0].Result[0].Location.Address.Label.split(', ')
-            console.log(feature.properties.instructions[i][0])
-            feature.properties.instructions[i] = feature.properties.instructions[i][0]
+            let i = 0
+            if (response.data.Response.View[0].Result[0]['MatchLevel'] === 'houseNumber') i = 0
+            else if (response.data.Response.View[0].Result.length >= 2) i = 1
+            feature.properties.instructions[index] = response.data.Response.View[0].Result[i].Location.Address.Label.split(', ')
+            feature.properties.instructions[index] = feature.properties.instructions[index][0]
           })
         }
       }
